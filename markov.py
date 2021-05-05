ae7f2c98d42ecbecbf42de5fd3ade274653812f7
Ben Rucker <brucker117@gmail.com>
2021-05-05 17:07:45 -0400

    strip whitespace from id from file

--- markov.py
+++ markov.py
@@ -243,7 +243,7 @@ def invalid_file(filename, gid):
     with open(path) as f:
         for _id in f:
             print('checking this id:', _id, 'against this filename:\n', filename)
-            if _id in filename:
+            if _id.strip() in filename:
                 return True
     return False
 
